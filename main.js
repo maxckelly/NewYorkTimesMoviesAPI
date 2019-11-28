1e827fb1f5c2b8bd6ad0b53f7fd3a4e526bb43ef
Max Kelly <max@revelthat.com>
2019-11-28 21:48:54 +1100

    displaying movie image

--- main.js
+++ main.js
@@ -10,24 +10,26 @@ input.addEventListener('input', (event) => {
   
   if (movies.children) {
     movies.innerHTML = null
-  }
+  };
 
   axios.get(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${input.value}&api-key=xIWGFzxCIlvycOEDzOPJcriGp2MwnoB4`).then((response) => {
-    console.log(response);
+
+  if (response.status === 429) {
+    return alert("to many requests");
+  };
+
+    // console.log(response);
     let movieData = response.data.results
 
     movieData.forEach((movie) => {
-    
+      
+      console.log(movie.multimedia.src);
       let movieTitle = document.createElement('h3');
       let movieDescription = document.createElement('p');
       movieTitle.innerHTML += movie.display_title;
       movieDescription.innerHTML += movie.headline;
       document.querySelector('.movies').appendChild(movieTitle);
       document.querySelector('.movies').appendChild(movieDescription);
-
-      // let movieHeadLine = document.createElement('p');
-      // movieHeadLine.innerHTML += movie.headline;
-      // document.querySelector('.movie-description').appendChild(movieHeadLine);
-    }) 
-  });
+    })
+  }) 
 });
